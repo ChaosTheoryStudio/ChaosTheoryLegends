ade1168ed8eacc509785b14ef2c8bf20ae24e77e
NicoPlayer <greauxnicolas@hotmail.com>
2025-05-04 21:43:46 -0400

    Bullet - Start

---
+++ ChaosTheoryLegends/Assets/Scripts/BulletHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BulletHandler : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D Rigidbody;
+    [SerializeField] private GameObject TestPrefab;
+
+    [SerializeField] private Transform BulletsParent;
+    [SerializeField] private Camera PlayerCamera;
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (!context.started)
+            return;
+
+        Vector2 realPos = PlayerCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        GameObject obj = Instantiate(TestPrefab, BulletsParent);
+        obj.transform.position = realPos;
+    }
+}
